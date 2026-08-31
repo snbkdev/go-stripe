02f0f7e52d3f0151399e65f2f2185f7ab2a44016
Asanbek09 <samudinua@gmail.com>
2026-08-31 16:44:37 +0600

    Админка для настройки пользователей

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -428,3 +428,17 @@ func (app *application) ShowSubscription(w http.ResponseWriter, r *http.Request)
 		app.errorLog.Println(err)
 	}
 }
+
+// AllUsers shows the all users page
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "all-users", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+// OneUser shows one admin user for add/delete/edit
+func (app *application) OneUser(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "one-user", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
